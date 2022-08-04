b7d866ec42bbe00d54d1bec9ee3b5adcaf1ab84d
Stefan Thomas <justmoon@members.fsf.org>
2022-08-04 13:43:15 -0400

    feat(oer): improve parsing error messages

--- packages/lib-oer/src/utils/errors.ts
+++ packages/lib-oer/src/utils/errors.ts
@@ -1,10 +1,29 @@
+import { uint8ArrayToHex } from "./hex"
+
+const PARSE_ERROR_CONTEXT_BYTES = 20
+
 export class ParseError extends Error {
   constructor(
     message: string,
     readonly data: Uint8Array,
     readonly offset: number
   ) {
-    super(message)
+    const hexExcerptRange = [
+      Math.max(0, offset - PARSE_ERROR_CONTEXT_BYTES),
+      Math.min(data.length, offset + PARSE_ERROR_CONTEXT_BYTES),
+    ] as const
+
+    const hexExcerpt = uint8ArrayToHex(
+      data.slice(hexExcerptRange[0], hexExcerptRange[1])
+    )
+
+    const formattedMessage = `${message}\n\n  ${
+      hexExcerptRange[0] === 0 ? " " : "…"
+    } ${hexExcerpt} ${
+      hexExcerptRange[1] === data.length ? " " : "…"
+    }\n    ${"".padEnd((offset - hexExcerptRange[0]) * 3, " ")}^^`
+
+    super(formattedMessage)
 
     this.name = "ParseError"
   }
