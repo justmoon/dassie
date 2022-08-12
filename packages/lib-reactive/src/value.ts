1821a5345e7ca2521c3dc1f2bed510ca5aa89f27
Stefan Thomas <justmoon@members.fsf.org>
2022-08-12 15:08:43 -0400

    feat(reactive): throw when trying to access an uninitialized value

--- packages/lib-reactive/src/value.ts
+++ packages/lib-reactive/src/value.ts
@@ -1,7 +1,14 @@
 import { isObject } from "@dassie/lib-type-utils"
 
 import { Effect, runEffect } from "./effect"
-import { FactoryNameSymbol, InitSymbol, Reactor } from "./reactor"
+import { LifecycleScope } from "./internal/lifecycle-scope"
+import {
+  AsyncDisposer,
+  DisposeSymbol,
+  FactoryNameSymbol,
+  InitSymbol,
+  Reactor,
+} from "./reactor"
 import { Store, createStore } from "./store"
 
 export const ValueSymbol = Symbol("das:reactive:value")
@@ -18,16 +25,38 @@ export type Value<TInstance> = Store<TInstance> & {
   [ValueSymbol]: true
 
   [InitSymbol]: (reactor: Reactor) => void
+  [DisposeSymbol]: AsyncDisposer
 }
 
-export const createValue = <T>(effect: Effect<undefined, T>): Value<T> => {
-  // It's ok to temporarily put undefined in the store since we will immediately replace it in the initialization function
-  const store = createStore<T>(undefined as unknown as T)
+/**
+ * Symbol which is used as the initial value for the store. This should never be visible to the user, since the store is immediately initialized by running the effect.
+ *
+ * The only way to get this value from the store is to access it from within the effect that initializes it which is a circular dependency and therefore a bug.
+ *
+ * To catch this case, we override the store's read() method and throw an error if it is called before the store has been initialized.
+ */
+const UninitializedSymbol = Symbol("das:reactive:uninitialized")
+
+export const createValue = <T>(effect: Effect<Value<T>, T>): Value<T> => {
+  const store = createStore<T>(UninitializedSymbol as unknown as T)
+  const lifecycle = new LifecycleScope()
   const value: Value<T> = {
     ...store,
+    read: () => {
+      throw new Error(
+        "Value has not been initialized. This indicates that you tried to read this value during the execution of its own initializer, i.e. a circular dependency."
+      )
+    },
     [ValueSymbol]: true,
     [InitSymbol]: (reactor: Reactor) => {
-      runEffect(reactor, effect, undefined, reactor, (newValue) => {
+      reactor.onCleanup(async () => {
+        await lifecycle.dispose()
+      })
+      runEffect(reactor, effect, value, lifecycle, (newValue) => {
+        // We had temporarily overridden the read method to catch circular dependencies. Restore it now.
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        if (value.read !== store.read) value.read = store.read
+
         if (store.read() !== newValue) {
           store.emit(() => newValue)
         }
@@ -37,6 +66,9 @@ export const createValue = <T>(effect: Effect<undefined, T>): Value<T> => {
           error,
         })
       })
+    },
+    [DisposeSymbol]: () => {
+      return lifecycle.dispose()
     },
   }
 
