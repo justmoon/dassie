191f0fb24e28e7c7c148f8db5996adb486554782
Stefan Thomas <justmoon@members.fsf.org>
2022-08-12 22:52:04 -0400

    feat(reactive): add support for topics in react

--- packages/lib-reactive-react/src/index.ts
+++ packages/lib-reactive-react/src/index.ts
@@ -2,15 +2,36 @@ import {
   createContext,
   useCallback,
   useContext,
+  useEffect,
   useSyncExternalStore,
 } from "react"
 
-import { Reactor, StoreFactory, createReactor } from "@dassie/lib-reactive"
+import {
+  Reactor,
+  StoreFactory,
+  TopicFactory,
+  createReactor,
+} from "@dassie/lib-reactive"
 
 export const createReactiveHooks = () => {
   const ReactorContext = createContext<Reactor>(createReactor(() => undefined))
 
   const useReactor = () => useContext(ReactorContext)
+
+  const useTopic = <TMessage>(
+    topicFactory: TopicFactory<TMessage>,
+    callback: (message: TMessage) => void
+  ) => {
+    const reactor = useContext(ReactorContext)
+
+    useEffect(() => {
+      const disposeSubscription = reactor.useContext(topicFactory).on(callback)
+      return () => {
+        disposeSubscription()
+        reactor.disposeContext(topicFactory)
+      }
+    }, [topicFactory, callback])
+  }
 
   const useStore = <TState>(
     storeFactory: StoreFactory<TState>
@@ -38,10 +59,11 @@ export const createReactiveHooks = () => {
   return {
     Provider: ReactorContext.Provider,
     useReactor,
+    useTopic,
     useStore,
   }
 }
 
-const { Provider, useReactor, useStore } = createReactiveHooks()
+const { Provider, useReactor, useTopic, useStore } = createReactiveHooks()
 
-export { Provider, useReactor, useStore }
+export { Provider, useReactor, useTopic, useStore }
