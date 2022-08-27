34bdebab234c01f57366d2da25f36ea43ce92977
Stefan Thomas <justmoon@members.fsf.org>
2022-08-27 14:56:52 -0400

    feat(http): add helper for JSON responses

--- packages/lib-http-server/src/responses.ts
+++ packages/lib-http-server/src/responses.ts
@@ -8,3 +8,12 @@ export const respondPlainly = (
   response.writeHead(statusCode, { "Content-Type": "text/plain" })
   response.end(message)
 }
+
+export const respondJson = (
+  response: ServerResponse,
+  statusCode: number,
+  data: unknown
+) => {
+  response.writeHead(statusCode, { "Content-Type": "application/json" })
+  response.end(JSON.stringify(data))
+}
