0569b7c5fb73cf50456c7b7675c75f89fbad2495
Stefan Thomas <justmoon@members.fsf.org>
2022-08-27 12:24:22 -0400

    fix(dev): exit rpc websocket server more cleanly

--- packages/app-dev/src/backend/effects/serve-rpc.ts
+++ packages/app-dev/src/backend/effects/serve-rpc.ts
@@ -1,17 +1,19 @@
-import type { EffectContext } from "@dassie/lib-reactive"
 import { applyWSSHandler } from "@trpc/server/adapters/ws"
 import { WebSocketServer } from "ws"
+
+import type { EffectContext } from "@dassie/lib-reactive"
 
 import { AppRouter, appRouter } from "../rpc-routers/app-router"
 
 export const listenForRpcWebSocket = (sig: EffectContext) => {
   const wss = new WebSocketServer({ port: 10_001 })
-  applyWSSHandler<AppRouter>({
+  const { broadcastReconnectNotification } = applyWSSHandler<AppRouter>({
     wss,
     router: appRouter,
     createContext: () => sig.reactor,
   })
   sig.onCleanup(() => {
+    broadcastReconnectNotification()
     wss.close()
   })
 }
