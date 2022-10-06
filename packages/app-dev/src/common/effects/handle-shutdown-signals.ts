700eb881c1d1e8c56513fbecdba1f8711243e2dc
Stefan Thomas <justmoon@members.fsf.org>
2022-10-06 17:06:18 -0400

    feat(dev): add a way to see open handles preventing shutdown

--- packages/app-dev/src/common/effects/handle-shutdown-signals.ts
+++ packages/app-dev/src/common/effects/handle-shutdown-signals.ts
@@ -1,10 +1,21 @@
+import wtfnode from "wtfnode"
+
 import type { EffectContext } from "@dassie/lib-reactive"
 
 export const handleShutdownSignals = (sig: EffectContext) => {
   const onShutdown = () => {
-    sig.reactor.dispose().catch((error: unknown) => {
-      console.error("failed to dispose reactor", { error })
-    })
+    sig.reactor
+      .dispose()
+      .then(() => {
+        setTimeout(() => {
+          if (process.env["DASSIE_DEV_OPEN_HANDLES"]) {
+            wtfnode.dump()
+          }
+        })
+      })
+      .catch((error: unknown) => {
+        console.error("failed to dispose reactor", { error })
+      })
   }
 
   process.on("SIGTERM", onShutdown)
