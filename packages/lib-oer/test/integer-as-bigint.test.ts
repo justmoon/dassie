5eefa4b9ddfb2eb9895ae29dc9fddeed8b085a62
Nicholas Dudfield <ndudfield@gmail.com>
2022-12-05 13:51:17 +0700

    chore: dedupe test code and add coverage package

--- packages/lib-oer/test/integer-as-bigint.test.ts
+++ packages/lib-oer/test/integer-as-bigint.test.ts
@@ -10,13 +10,52 @@ import {
   uint16Bigint,
   uint32Bigint,
   uint64Bigint,
-} from "../src/integer-as-bigint"
-import { hexToUint8Array } from "../src/utils/hex"
+} from "../src"
+import { hexToUint8Array } from "../src"
 import { parsedOk, serializedOk } from "./utils/result"
 import {
   getFixedLengthSamples,
   getVariableLengthSamples,
 } from "./utils/sample-numbers"
+
+function defineCommonTests(schema: ReturnType<typeof integerAsBigint>) {
+  test("should return an object", ({ expect }) => {
+    expect(schema).toBeTypeOf("object")
+  })
+
+  test("should reject a zero-length zero", ({ expect }) => {
+    const value = schema.parse(hexToUint8Array("00"))
+    expect(value).toMatchInlineSnapshot(`
+      {
+        "error": [ParseError: unable to read variable length integer - length must not be 0
+
+          00  
+          ^^],
+        "success": false,
+      }
+    `)
+  })
+
+  test("should parse a zero", ({ expect }) => {
+    const value = schema.parse(hexToUint8Array("0100"))
+    expect(value).toEqual(parsedOk(2, 0n))
+  })
+
+  test("should serialize a zero", ({ expect }) => {
+    const value = schema.serialize(0n)
+    expect(value).toEqual(serializedOk("0100"))
+  })
+
+  test("should parse a bigint with one byte", ({ expect }) => {
+    const value = schema.parse(hexToUint8Array("01 12"))
+    expect(value).toEqual(parsedOk(2, 0x12n))
+  })
+
+  test("should serialize a bigint with one byte", ({ expect }) => {
+    const value = schema.serialize(0x12n)
+    expect(value).toEqual(serializedOk("01 12"))
+  })
+}
 
 describe("integerAsBigint", () => {
   test("should be a function", ({ expect }) => {
@@ -26,34 +65,7 @@ describe("integerAsBigint", () => {
   describe("schema variable-length signed integer", () => {
     const schema = integerAsBigint()
 
-    test("should return an object", ({ expect }) => {
-      expect(schema).toBeTypeOf("object")
-    })
-
-    test("should reject a zero-length zero", ({ expect }) => {
-      const value = schema.parse(hexToUint8Array("00"))
-      expect(value).toMatchSnapshot()
-    })
-
-    test("should parse a zero", ({ expect }) => {
-      const value = schema.parse(hexToUint8Array("0100"))
-      expect(value).toEqual(parsedOk(2, 0n))
-    })
-
-    test("should serialize a zero", ({ expect }) => {
-      const value = schema.serialize(0n)
-      expect(value).toEqual(serializedOk("0100"))
-    })
-
-    test("should parse a bigint with one byte", ({ expect }) => {
-      const value = schema.parse(hexToUint8Array("01 12"))
-      expect(value).toEqual(parsedOk(2, 0x12n))
-    })
-
-    test("should serialize a bigint with one byte", ({ expect }) => {
-      const value = schema.serialize(0x12n)
-      expect(value).toEqual(serializedOk("01 12"))
-    })
+    defineCommonTests(schema)
 
     test("should serialize a negative bigint with one byte", ({ expect }) => {
       const value = schema.serialize(-0x12n)
@@ -64,7 +76,30 @@ describe("integerAsBigint", () => {
       const testValue = 0x12_34_56_78_9a_bc_de_f0_12_34_56_78_9a_bc_de_f0n
 
       const value = schema.serialize(testValue)
-      expect(value).toMatchSnapshot()
+      expect(value).toMatchInlineSnapshot(`
+        {
+          "success": true,
+          "value": Uint8Array [
+            16,
+            18,
+            52,
+            86,
+            120,
+            154,
+            188,
+            222,
+            240,
+            18,
+            52,
+            86,
+            120,
+            154,
+            188,
+            222,
+            240,
+          ],
+        }
+      `)
     })
 
     describe.each(getVariableLengthSamples(true))(
@@ -85,46 +120,46 @@ describe("integerAsBigint", () => {
 
   describe("schema variable-length unsigned integer", () => {
     const schema = integerAsBigint([0n, undefined])
-
-    test("should return an object", ({ expect }) => {
-      expect(schema).toBeTypeOf("object")
-    })
-
-    test("should reject a zero-length zero", ({ expect }) => {
-      const value = schema.parse(hexToUint8Array("00"))
-      expect(value).toMatchSnapshot()
-    })
-
-    test("should parse a zero", ({ expect }) => {
-      const value = schema.parse(hexToUint8Array("0100"))
-      expect(value).toEqual(parsedOk(2, 0n))
-    })
-
-    test("should serialize a zero", ({ expect }) => {
-      const value = schema.serialize(0n)
-      expect(value).toEqual(serializedOk("0100"))
-    })
-
-    test("should parse a bigint with one byte", ({ expect }) => {
-      const value = schema.parse(hexToUint8Array("01 12"))
-      expect(value).toEqual(parsedOk(2, 0x12n))
-    })
-
-    test("should serialize a bigint with one byte", ({ expect }) => {
-      const value = schema.serialize(0x12n)
-      expect(value).toEqual(serializedOk("01 12"))
-    })
+    defineCommonTests(schema)
 
     test("should refuse to serialize a negative bigint", ({ expect }) => {
       const value = schema.serialize(-0x12n)
-      expect(value).toMatchSnapshot()
+      expect(value).toMatchInlineSnapshot(`
+        {
+          "error": [SerializeError: expected unsigned bigint, got bigint: -18],
+          "success": false,
+        }
+      `)
     })
 
     test("should serialize a very large number", ({ expect }) => {
       const testValue = 0x12_34_56_78_9a_bc_de_f0_12_34_56_78_9a_bc_de_f0n
 
       const value = schema.serialize(testValue)
-      expect(value).toMatchSnapshot()
+      expect(value).toMatchInlineSnapshot(`
+        {
+          "success": true,
+          "value": Uint8Array [
+            16,
+            18,
+            52,
+            86,
+            120,
+            154,
+            188,
+            222,
+            240,
+            18,
+            52,
+            86,
+            120,
+            154,
+            188,
+            222,
+            240,
+          ],
+        }
+      `)
     })
 
     describe.each(getVariableLengthSamples(false))(
@@ -184,22 +219,48 @@ describe("integerAsBigint", () => {
 
     test("should not parse a number that is too high", ({ expect }) => {
       const result = schema.parse(hexToUint8Array("09"))
-      expect(result).toMatchSnapshot()
+      expect(result).toMatchInlineSnapshot(`
+        {
+          "error": [ParseError: unable to read fixed length integer of size 1 bytes - value 9 is greater than maximum value 8
+
+            09  
+            ^^],
+          "success": false,
+        }
+      `)
     })
 
     test("should not serialize a number that is too high", ({ expect }) => {
       const result = schema.serialize(9n)
-      expect(result).toMatchSnapshot()
+      expect(result).toMatchInlineSnapshot(`
+        {
+          "error": [SerializeError: integer must be <= 8],
+          "success": false,
+        }
+      `)
     })
 
     test("should not parse a number that is too low", ({ expect }) => {
       const result = schema.parse(hexToUint8Array("01"))
-      expect(result).toMatchSnapshot()
+      expect(result).toMatchInlineSnapshot(`
+        {
+          "error": [ParseError: unable to read fixed length integer of size 1 bytes - value 1 is less than minimum value 5
+
+            01  
+            ^^],
+          "success": false,
+        }
+      `)
     })
 
     test("should not serialize a number that is too low", ({ expect }) => {
       const result = schema.serialize(1n)
-      expect(result).toMatchSnapshot()
+      expect(result).toMatchInlineSnapshot(`
+        {
+          "error": [SerializeError: integer must be >= 5],
+          "success": false,
+        }
+      `)
     })
   })
 })
