56fa88450d79960e54f6262597c863206deeb0e9
Stefan Thomas <justmoon@members.fsf.org>
2022-12-25 13:17:51 -0400

    refactor(node): use onAsync for built-in error handling in sendOutgoingPackets

--- packages/app-node/src/backend/ilp-connector/send-outgoing-packets.ts
+++ packages/app-node/src/backend/ilp-connector/send-outgoing-packets.ts
@@ -1,25 +1,17 @@
-import { createLogger } from "@dassie/lib-logger"
 import type { EffectContext } from "@dassie/lib-reactive"
 
 import { ilpRoutingTableSignal } from "./signals/ilp-routing-table"
 import { outgoingIlpPacketBuffer } from "./topics/outgoing-ilp-packet"
 
-const logger = createLogger("das:node:send-outgoing-packets")
-
 export const sendOutgoingPackets = (sig: EffectContext) => {
   const ilpClientMap = sig.use(ilpRoutingTableSignal)
 
-  sig.use(outgoingIlpPacketBuffer).on((event) => {
+  sig.onAsync(outgoingIlpPacketBuffer, async (event) => {
     const client = ilpClientMap.read().lookup(event.destination)
     if (!client) {
       throw new Error(`No routing table entry found for ${event.destination}`)
     }
 
-    Promise.resolve(client.sendPacket(event)).catch((error: unknown) => {
-      logger.error("failed to send outgoing packet", {
-        error,
-        to: event.destination,
-      })
-    })
+    await client.sendPacket(event)
   })
 }
