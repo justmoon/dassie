e925fe634d453a4489179c8ca0a6ce4f36c08833
Stefan Thomas <justmoon@members.fsf.org>
2023-01-01 18:11:18 -0400

    fix(dev): fix reloading on code change

--- packages/app-dev/src/backend/effects/handle-file-change.ts
+++ packages/app-dev/src/backend/effects/handle-file-change.ts
@@ -6,6 +6,7 @@ import { createLogger } from "@dassie/lib-logger"
 import { EffectContext, createTopic } from "@dassie/lib-reactive"
 
 import { logsStore } from "../../common/stores/logs"
+import { viteNodeService } from "../services/vite-node-server"
 import { viteService } from "../services/vite-server"
 
 const logger = createLogger("das:dev:handle-file-change")
@@ -18,12 +19,17 @@ export const fileChangeTopic = () => createTopic()
 
 export const handleFileChange = async (sig: EffectContext) => {
   const viteServer = await sig.get(viteService)
+  const viteNodeServer = await sig.get(viteNodeService)
 
   if (!viteServer) return
 
   const onFileChange = (file: string) => {
     const { config, moduleGraph } = viteServer
     const shortFile = getShortName(file, config.root)
+
+    // The cache in vite-node doesn't correctly invalidate, so we need to clear it on each file change.
+    // It still helps a lot with performance because we are compiling the code once and then re-using it for every running process.
+    if (viteNodeServer) viteNodeServer.fetchCache = new Map()
 
     const mods = moduleGraph.getModulesByFile(file)
 
