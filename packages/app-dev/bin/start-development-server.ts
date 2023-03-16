5d0c5c947342c934d3ff32fcd45102f50757259c
Stefan Thomas <justmoon@members.fsf.org>
2023-03-16 15:16:30 -0400

    feat(dev): automatically reload dev server if files change

--- packages/app-dev/bin/start-development-server.ts
+++ packages/app-dev/bin/start-development-server.ts
@@ -1,3 +1,21 @@
+import type { Reactor } from "@dassie/lib-reactive"
+
 import start from "../src"
 
-void start()
+interface GlobalWithReactor {
+  reactor?: Reactor | undefined
+  latest?: symbol
+}
+
+const context = global as GlobalWithReactor
+
+const ourSymbol = Symbol()
+context.latest = ourSymbol
+
+if (context.reactor) {
+  await context.reactor.dispose()
+}
+
+if (context.latest === ourSymbol) {
+  context.reactor = start()
+}
