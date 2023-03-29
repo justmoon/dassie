38bfb7ffb2dbae358205258393fcb4234ae5f29a
Stefan Thomas <justmoon@members.fsf.org>
2023-03-29 08:04:59 -0400

    fix(node): make peers computation result immutable

--- packages/app-node/src/backend/peer-protocol/computed/peers.ts
+++ packages/app-node/src/backend/peer-protocol/computed/peers.ts
@@ -1,45 +1,66 @@
+import produce, { enableMapSet } from "immer"
+
 import { createActor } from "@dassie/lib-reactive"
 
 import { NodeTableKey, nodeTableStore } from "../stores/node-table"
 
+enableMapSet()
+
 export const peersComputation = () => {
-  const peersSet = new Set<NodeTableKey>()
+  const emptySet = new Set<NodeTableKey>()
   const actor = createActor((sig) => {
     const nodeTable = sig.use(nodeTableStore)
 
-    nodeTable.changes.on(([actionId, parameters]) => {
-      const priorSize = peersSet.size
+    const initialSet = new Set<NodeTableKey>()
 
-      switch (actionId) {
-        case "addNode": {
-          const { peerState, subnetId, nodeId } = parameters[0]
-          if (peerState.id === "peered" || peerState.id === "request-peering") {
-            peersSet.add(`${subnetId}.${nodeId}`)
-          }
-          break
-        }
-        case "updateNode": {
-          const nodeKey = parameters[0]
-          const { peerState } = parameters[1]
-          if (
-            peerState?.id === "peered" ||
-            peerState?.id === "request-peering"
-          ) {
-            peersSet.add(nodeKey)
-          } else if (peerState?.id === "none") {
-            peersSet.delete(nodeKey)
-          }
-          break
-        }
+    for (const node of nodeTable.read().values()) {
+      if (
+        node.peerState.id === "peered" ||
+        node.peerState.id === "request-peering"
+      ) {
+        initialSet.add(`${node.subnetId}.${node.nodeId}`)
       }
+    }
 
-      if (peersSet.size !== priorSize) {
-        actor.emit(peersSet)
+    nodeTable.changes.on(([actionId, parameters]) => {
+      const peersSet = actor.read()
+      const priorSize = actor.read().size
+
+      const newSet = produce(peersSet, (draft) => {
+        switch (actionId) {
+          case "addNode": {
+            const { peerState, subnetId, nodeId } = parameters[0]
+            if (
+              peerState.id === "peered" ||
+              peerState.id === "request-peering"
+            ) {
+              draft.add(`${subnetId}.${nodeId}`)
+            }
+            break
+          }
+          case "updateNode": {
+            const nodeKey = parameters[0]
+            const { peerState } = parameters[1]
+            if (
+              peerState?.id === "peered" ||
+              peerState?.id === "request-peering"
+            ) {
+              draft.add(nodeKey)
+            } else if (peerState?.id === "none") {
+              draft.delete(nodeKey)
+            }
+            break
+          }
+        }
+      })
+
+      if (newSet.size !== priorSize) {
+        actor.write(newSet)
       }
     })
 
-    return peersSet
-  }, peersSet)
+    return initialSet
+  }, emptySet)
 
   return actor
 }
