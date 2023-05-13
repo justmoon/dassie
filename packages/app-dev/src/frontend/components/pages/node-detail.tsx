054adc22f2b0fb4747bc8c280aa506fb30492192
Stefan Thomas <justmoon@members.fsf.org>
2023-05-13 15:17:24 -0400

    feat(dev): remove balances from node detail view
    This code won't be compatible with the new accounting system and so needs to be removed.

--- packages/app-dev/src/frontend/components/pages/node-detail.tsx
+++ packages/app-dev/src/frontend/components/pages/node-detail.tsx
@@ -4,7 +4,6 @@ import { FaLink } from "react-icons/fa"
 import { format } from "timeago.js"
 import { Link } from "wouter"
 
-import type { NodeTableKey } from "@dassie/app-node/src/backend/peer-protocol/stores/node-table"
 import { selectBySeed } from "@dassie/lib-logger"
 
 import { convertVanityNodeIdToFriendly } from "../../../common/utils/vanity-node-id-to-friendly"
@@ -68,20 +67,17 @@ const NodeLink = ({ nodeId }: BasicNodeElementProperties) => {
 const NodeTable = ({ nodeId }: BasicNodeElementProperties) => {
   const nodeTable = useNodeRemoteSignal(nodeId, "nodeTable")
   const routingTable = useNodeRemoteSignal(nodeId, "routingTable")
-  const balanceTable = useNodeRemoteSignal(nodeId, "balanceTable")
 
-  if (!nodeTable.data || !routingTable.data || !balanceTable.data) return null
+  if (!nodeTable.data || !routingTable.data) return null
 
   const sortedNodeTable = [...nodeTable.data.values()]
     .map((node) => {
-      const nodeKey: NodeTableKey = `${node.subnetId}.${node.nodeId}`
       const routingTableEntry = routingTable.data.get(node.nodeId)
 
       return {
         ...node,
         distance: routingTableEntry?.distance ?? Number.POSITIVE_INFINITY,
         nextHopNodes: routingTable.data.get(node.nodeId)?.firstHopOptions ?? [],
-        balance: balanceTable.data.get(nodeKey),
       }
     })
     // Sort first by distance, then by nodeId
@@ -105,7 +101,6 @@ const NodeTable = ({ nodeId }: BasicNodeElementProperties) => {
             <th className="text-left">Peering</th>
             <th>Next Hop Options</th>
             <th>Neighbors</th>
-            <th>Balance</th>
             <th className="text-left">Last Seen</th>
           </tr>
         </thead>
@@ -133,7 +128,6 @@ const NodeTable = ({ nodeId }: BasicNodeElementProperties) => {
                     ))}
                   </div>
                 </td>
-                <td>{node.balance?.balance.toString() ?? ""}</td>
                 {node.peerState.id === "none" ? (
                   <td></td>
                 ) : (
