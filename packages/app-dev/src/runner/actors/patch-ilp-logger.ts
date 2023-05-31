c732f45ba0809ca46d94c2dcdf11189f9ed7527e
Stefan Thomas <justmoon@members.fsf.org>
2023-05-31 15:17:49 -0400

    feat(dev): colorize namespace for legacy ilp-logger logs

--- packages/app-dev/src/runner/actors/patch-ilp-logger.ts
+++ packages/app-dev/src/runner/actors/patch-ilp-logger.ts
@@ -1,5 +1,6 @@
 import { createRequire } from "node:module"
 
+import { defaultTheme, selectBySeed } from "@dassie/lib-logger"
 import { createActor } from "@dassie/lib-reactive"
 
 export const patchIlpLogger = () =>
@@ -11,6 +12,17 @@ export const patchIlpLogger = () =>
     const loggerRequire = createRequire(streamRequire.resolve("ilp-logger"))
     const debug = loggerRequire("debug") as unknown
     ;(debug as { log: typeof console.debug }).log = (
+      message: string,
       ...parameters: unknown[]
-    ) => console.debug(...parameters)
+    ) => {
+      const firstSpace = message.indexOf(" ")
+      if (firstSpace !== -1) {
+        const namespace = message.slice(0, firstSpace)
+        message = `${selectBySeed(
+          defaultTheme.colors,
+          namespace
+        )(namespace)} ${message.slice(firstSpace + 1)}`
+      }
+      console.debug(message, ...parameters)
+    }
   })
