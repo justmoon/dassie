d6f749bfe174e32061fe83206375541f9f763d11
Stefan Thomas <justmoon@members.fsf.org>
2023-07-02 12:27:25 -0400

    refactor(node): remove unused packet result function

--- packages/app-node/src/backend/accounting/functions/apply-interledger-packet.ts
+++ packages/app-node/src/backend/accounting/functions/apply-interledger-packet.ts
@@ -24,27 +24,3 @@ export const applyPacketPrepareToLedger = (
 
   return ledger.createTransfer(transfer)
 }
-
-export const applyPacketResultToLedger = (
-  ledger: Ledger,
-  accountPath: string,
-  packet: IlpPreparePacket,
-  result: "fulfill" | "reject"
-) => {
-  const base64Condition = Buffer.from(packet.executionCondition).toString(
-    "base64"
-  )
-  const transfer = ledger.getPendingTransfer(
-    `${accountPath};${base64Condition}`
-  )
-
-  if (!transfer) {
-    throw new Error(`No pending transfer for condition ${base64Condition}`)
-  }
-
-  if (result === "fulfill") {
-    ledger.postPendingTransfer(transfer)
-  } else {
-    ledger.voidPendingTransfer(transfer)
-  }
-}
