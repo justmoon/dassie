e50e979f0016d1c01181d07db5c8d45646afee2c
Stefan Thomas <justmoon@members.fsf.org>
2023-07-16 13:59:21 -0400

    fix(build): make sure build succeeds if dist directory doesn't exist

--- packages/app-build/src/steps/delete-output-path.ts
+++ packages/app-build/src/steps/delete-output-path.ts
@@ -1,7 +1,17 @@
-import { rm } from "node:fs/promises"
+import { access, rm } from "node:fs/promises"
+
+import { isErrorWithCode } from "@dassie/lib-type-utils"
 
 import { PATH_DIST } from "../constants/paths"
 
 export const deleteOutputPath = async () => {
-  await rm(PATH_DIST, { recursive: true })
+  try {
+    await access(PATH_DIST)
+
+    await rm(PATH_DIST, { recursive: true })
+  } catch (error: unknown) {
+    if (isErrorWithCode(error, "ENOENT")) return
+
+    throw error
+  }
 }
