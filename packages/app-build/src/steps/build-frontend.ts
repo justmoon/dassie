a56fba6d4eb453f63320cd6d05282423be84a85c
Stefan Thomas <justmoon@members.fsf.org>
2023-07-23 10:16:35 -0400

    fix(build): make unocss work in production frontend
    It was wrong to load the vite config and pass it to build() because build() also loads the config from root. This caused all of the plugins to be loaded twice, which prevented unocss from working correctly.

--- packages/app-build/src/steps/build-frontend.ts
+++ packages/app-build/src/steps/build-frontend.ts
@@ -1,9 +1,6 @@
 import { build } from "vite"
 
-import assert from "node:assert"
 import { resolve } from "node:path"
-
-import viteConfigExport from "@dassie/app-node/src/frontend/vite.config"
 
 import {
   PATH_DIST_STAGING_SHARED,
@@ -14,28 +11,20 @@ export const buildFrontend = async (detailedVersion: string) => {
   const sourceDirectory = resolve(PATH_PACKAGE_APP_NODE, "src/frontend")
   const outputDirectory = resolve(PATH_DIST_STAGING_SHARED, "frontend")
 
-  const viteConfig = await viteConfigExport
-
-  assert(typeof viteConfig === "object", "Expected viteConfig to be an object")
-
   // vite-node sets NODE_ENV to "development" by default so we need to override
   // it, otherwise react/jsx-runtime will use the development version.
   process.env["NODE_ENV"] = "production"
 
   console.info()
   await build({
-    ...viteConfig,
     mode: "production",
     root: sourceDirectory,
     define: {
-      ...viteConfig.define,
       __DASSIE_VERSION__: JSON.stringify(detailedVersion),
     },
     build: {
-      ...viteConfig.build,
       outDir: outputDirectory,
       chunkSizeWarningLimit: 1024 * 1024,
-      minify: false,
     },
   })
 }
