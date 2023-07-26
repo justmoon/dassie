5245c11452e170732c43c39c9b73c3e5539f59e9
Stefan Thomas <justmoon@members.fsf.org>
2023-07-26 15:49:27 -0400

    feat(dev): add log level to log viewer

--- packages/app-dev/src/frontend/components/log-viewer/log-line.tsx
+++ packages/app-dev/src/frontend/components/log-viewer/log-line.tsx
@@ -6,11 +6,21 @@ interface LogLineProperties {
   log: IndexedLogLine
 }
 
+const LOG_LEVEL_COLORS = {
+  debug: "#7e84fa",
+  info: "#147af3",
+  warn: "#e8c600",
+  error: "#de3d82",
+} as const
+
 const LogLine = ({ log }: LogLineProperties) => {
   return (
     <div className="flex text-xs py-0.5" style={{ order: -log.index }}>
       <div className="font-mono flex-shrink-0 text-right px-2 text-gray-400 w-20">
         {(log.relativeTime / 1000).toFixed(3)}
+      </div>
+      <div className="w-8" style={{ color: LOG_LEVEL_COLORS[log.type] }}>
+        {log.type}
       </div>
       <div className="font-bold flex-shrink-0 text-center w-8">
         <NodeLink id={log.node} />
