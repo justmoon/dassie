f7c5b7e533e987aa9ebb2d1389c60a5545c3b980
Stefan Thomas <justmoon@members.fsf.org>
2023-11-07 09:05:44 -0400

    feat(node): provide own node id and public key in stats endpoint

--- packages/app-node/src/backend/statistics/register-statistics-http-handler.ts
+++ packages/app-node/src/backend/statistics/register-statistics-http-handler.ts
@@ -1,19 +1,28 @@
 import { respondJson } from "@dassie/lib-http-server"
 import { Reactor, createActor } from "@dassie/lib-reactive"
 
+import { DatabaseConfigStore } from "../config/database-config"
+import { NodePublicKeySignal } from "../crypto/computed/node-public-key"
 import { HttpsRouterServiceActor } from "../http-server/serve-https"
+import { NodeIdSignal } from "../ilp-connector/computed/node-id"
 import { PeersSignal } from "../peer-protocol/computed/peers"
 import { NodeTableStore } from "../peer-protocol/stores/node-table"
 
-export const RegisterStatisticsHttpHandlerActor = (reactor: Reactor) =>
-  createActor((sig) => {
+export const RegisterStatisticsHttpHandlerActor = (reactor: Reactor) => {
+  const nodeIdSignal = reactor.use(NodeIdSignal)
+  const nodeTableStore = reactor.use(NodeTableStore)
+  const peersSignal = reactor.use(PeersSignal)
+  const nodePublicKeySignal = reactor.use(NodePublicKeySignal)
+  const databaseConfigStore = reactor.use(DatabaseConfigStore)
+
+  return createActor((sig) => {
     const router = sig.get(HttpsRouterServiceActor)
 
     if (!router) return
 
     router.get("/stats", (_, response) => {
-      const nodeTable = reactor.use(NodeTableStore).read()
-      const peers = [...reactor.use(PeersSignal).read()]
+      const nodeTable = nodeTableStore.read()
+      const peers = [...peersSignal.read()]
         .map((nodeKey) => {
           const node = nodeTable.get(nodeKey)
 
@@ -33,7 +42,15 @@ export const RegisterStatisticsHttpHandlerActor = (reactor: Reactor) =>
 
       respondJson(response, 200, {
         version: __DASSIE_VERSION__,
+        node: {
+          id: nodeIdSignal.read(),
+          url: databaseConfigStore.read().url,
+          publicKey: Buffer.from(nodePublicKeySignal.read()).toString(
+            "base64url",
+          ),
+        },
         peers,
       })
     })
   })
+}
