7e208d52a804957181d0d6cad42b58fab3ce454d
Stefan Thomas <justmoon@members.fsf.org>
2023-11-08 17:55:56 -0400

    chore: change dev environment ports to not conflict with rafiki
    Rafiki's dev environment uses a bunch of ports in the 4000 range - this commit changes our port ranges to avoid those so developers can run both Rafiki and Dassie dev environments on the same machine at the same time.

--- packages/app-dev/src/backend/constants/ports.ts
+++ packages/app-dev/src/backend/constants/ports.ts
@@ -3,5 +3,5 @@ export const SNI_PROXY_PORT = 443
 export const DEBUG_UI_PORT = 10_000
 export const DEBUG_RPC_PORT = 10_001
 
-export const NODES_START_PORT = 4001
-export const NODES_DEBUG_START_PORT = 5001
+export const NODES_START_PORT = 5001
+export const NODES_DEBUG_START_PORT = 6001
