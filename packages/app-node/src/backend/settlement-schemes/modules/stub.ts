07aff3972be2ea7c5b11f5d78a528f7964a631b4
Stefan Thomas <justmoon@members.fsf.org>
2023-11-25 17:33:52 -0400

    feat(node): simulate underlying ledger balance in stub settlement

--- packages/app-node/src/backend/settlement-schemes/modules/stub.ts
+++ packages/app-node/src/backend/settlement-schemes/modules/stub.ts
@@ -1,6 +1,17 @@
 import { EMPTY_UINT8ARRAY } from "../../../common/constants/general"
 import { LedgerId } from "../../accounting/types/ledger-id"
+import { settlementStub as logger } from "../../logger/instances"
 import type { SettlementSchemeModule } from "../types/settlement-scheme-module"
+
+const INITIAL_BALANCE = 100_000_000_000n
+
+const ledger = {
+  id: "stub" as LedgerId,
+  currency: {
+    code: "USD",
+    scale: 9,
+  },
+}
 
 /**
  * The stub settlement scheme pretends to settle but does not actually do anything.
@@ -14,16 +25,14 @@ const stub = {
   supportedVersions: [1],
   realm: "test",
 
-  ledger: {
-    id: "stub" as LedgerId,
-    currency: {
-      code: "USD",
-      scale: 9,
-    },
-  },
+  ledger,
 
   // eslint-disable-next-line unicorn/consistent-function-scoping
-  behavior: () => {
+  behavior: ({ host }) => {
+    let balance = INITIAL_BALANCE
+
+    host.reportOnLedgerBalance({ ledgerId: ledger.id, balance })
+
     return {
       getPeeringInfo: () => {
         return {
@@ -47,13 +56,23 @@ const stub = {
         }
       },
       settle: ({ peerId, amount }) => {
-        console.info(`Sending settlement for ${amount} units to ${peerId}`)
+        if (amount > balance) {
+          logger.error("Insufficient balance", { amount, balance })
+          throw new Error("Insufficient balance")
+        }
+        logger.info(`Sending settlement for ${amount} units to ${peerId}`)
+
+        balance -= amount
+
         return {
           proof: new Uint8Array(),
         }
       },
       handleSettlement: ({ peerId, amount }) => {
-        console.info(`Received settlement for ${amount} units from ${peerId}`)
+        logger.info(`Received settlement for ${amount} units from ${peerId}`)
+
+        balance += amount
+
         return {
           result: "accept" as const,
         }
