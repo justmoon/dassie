6db77ce62c11a956973aa85024bd802f2f851c8d
Stefan Thomas <justmoon@members.fsf.org>
2023-12-25 16:35:19 -0400

    chore(oer): move sample buffer initialization into a function

--- packages/lib-oer/test/utils/sample-buffer.ts
+++ packages/lib-oer/test/utils/sample-buffer.ts
@@ -1,11 +1,17 @@
 import { createHmac } from "node:crypto"
 
-export const sampleBuffer = new Uint8Array(1024)
+export const sampleBuffer = initializeSampleBuffer()
 
-for (let index = 0; index < sampleBuffer.byteLength / 512; index++) {
-  const offset = index * 64
+function initializeSampleBuffer() {
+  const buffer = new Uint8Array(1024)
 
-  const seededPseudoRandom = createHmac("sha512", "xen-test-seed")
-  seededPseudoRandom.update(String(index))
-  sampleBuffer.set(seededPseudoRandom.digest(), offset)
+  for (let index = 0; index < buffer.byteLength / 512; index++) {
+    const offset = index * 64
+
+    const seededPseudoRandom = createHmac("sha512", "xen-test-seed")
+    seededPseudoRandom.update(String(index))
+    buffer.set(seededPseudoRandom.digest(), offset)
+  }
+
+  return buffer
 }
