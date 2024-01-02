97a3d932e8ebbb6de90435a5b1d52108e400754f
Stefan Thomas <justmoon@members.fsf.org>
2024-01-02 18:19:53 -0400

    feat(http): add cookie parser

---
+++ packages/lib-http-server/src/cookie.ts
@@ -0,0 +1,45 @@
+/* eslint-disable unicorn/no-hex-escape, unicorn/better-regex -- In this case, the regex is actually more readable without these rules.*/
+import { IncomingMessage } from "node:http"
+
+import { Middleware } from "./router"
+
+export type Cookies = Record<string, string>
+
+export const COOKIE_TOKEN_REGEX =
+  /^[\x21\x23-\x27\x2A-\x2B\x2D-\x2E\x30-\x39\x41-\x5A\x5E-\x7A\x7C\x7E]+$/
+
+export const COOKIE_VALUE_REGEX =
+  /^[\x21\x23-\x2B\x2D-\x3A\x3C-\x5B\x5D-\x7E]*$/
+
+export const cookie = ((request: {
+  headers: { cookie?: IncomingMessage["headers"]["cookie"] }
+}) => {
+  const cookies: Cookies = {}
+
+  const cookieHeader = request.headers.cookie
+  if (cookieHeader) {
+    for (const cookie of cookieHeader.split(";")) {
+      const parts = cookie.split("=").map((part) => part.trim()) as
+        | [string]
+        | [string, string]
+
+      if (parts[1] === undefined) continue
+
+      const name = parts[0]
+      const value =
+        parts[1].startsWith('"') && parts[1].endsWith('"')
+          ? parts[1].slice(1, -1)
+          : parts[1]
+
+      if (
+        COOKIE_TOKEN_REGEX.test(name) &&
+        cookies[name] === undefined &&
+        COOKIE_VALUE_REGEX.test(value)
+      ) {
+        cookies[name] = value
+      }
+    }
+  }
+
+  return { cookies }
+}) satisfies Middleware<object, { cookies: Cookies }>
---
+++ packages/lib-http-server/test/cookie.test.ts
@@ -0,0 +1,93 @@
+import { describe, test } from "vitest"
+
+import { cookie } from "../src"
+
+describe("cookie", () => {
+  test("should handle a request with no cookie header", ({ expect }) => {
+    const result = cookie({ headers: {} })
+    expect(result).toStrictEqual({ cookies: {} })
+  })
+
+  test("should handle a request with an empty cookie header", ({ expect }) => {
+    const result = cookie({ headers: { cookie: "" } })
+    expect(result).toStrictEqual({ cookies: {} })
+  })
+
+  test("should handle a request with a cookie header with one cookie", ({
+    expect,
+  }) => {
+    const result = cookie({ headers: { cookie: "a=b" } })
+    expect(result).toStrictEqual({ cookies: { a: "b" } })
+  })
+
+  test("should handle a request with a cookie header with two cookies", ({
+    expect,
+  }) => {
+    const result = cookie({ headers: { cookie: "a=b; c=d" } })
+    expect(result).toStrictEqual({ cookies: { a: "b", c: "d" } })
+  })
+
+  test("should ignore spaces", ({ expect }) => {
+    const result = cookie({ headers: { cookie: " a    = b ; c =    d " } })
+    expect(result).toStrictEqual({ cookies: { a: "b", c: "d" } })
+  })
+
+  test("should handle a request with a cookie header with a quoted cookie", ({
+    expect,
+  }) => {
+    const result = cookie({ headers: { cookie: 'a="b"' } })
+    expect(result).toStrictEqual({ cookies: { a: "b" } })
+  })
+
+  test("should use the first value if a cookie is set twice", ({ expect }) => {
+    const result = cookie({ headers: { cookie: "a=b; a=c" } })
+    expect(result).toStrictEqual({ cookies: { a: "b" } })
+  })
+
+  test("should allow a cookie with an empty value", ({ expect }) => {
+    const result = cookie({ headers: { cookie: "a=; b=a" } })
+    expect(result).toStrictEqual({ cookies: { a: "", b: "a" } })
+  })
+
+  test("should ignore cookies containing whitespace in the token", ({
+    expect,
+  }) => {
+    const result = cookie({ headers: { cookie: "a b=c" } })
+    expect(result).toStrictEqual({ cookies: {} })
+  })
+
+  test("should ignore cookies containing whitespace in the value", ({
+    expect,
+  }) => {
+    const result = cookie({ headers: { cookie: "a=b c" } })
+    expect(result).toStrictEqual({ cookies: {} })
+  })
+
+  test("should ignore cookies containing whitespace in the token", ({
+    expect,
+  }) => {
+    const result = cookie({ headers: { cookie: "a b=c" } })
+    expect(result).toStrictEqual({ cookies: {} })
+  })
+
+  test("should ignore cookies containing whitespace in the value", ({
+    expect,
+  }) => {
+    const result = cookie({ headers: { cookie: "a=b c" } })
+    expect(result).toStrictEqual({ cookies: {} })
+  })
+
+  test("should ignore cookies containing invalid characters in the token", ({
+    expect,
+  }) => {
+    const result = cookie({ headers: { cookie: "a,b=c" } })
+    expect(result).toStrictEqual({ cookies: {} })
+  })
+
+  test("should ignore cookies containing invalid characters in the value", ({
+    expect,
+  }) => {
+    const result = cookie({ headers: { cookie: "a=b,c" } })
+    expect(result).toStrictEqual({ cookies: {} })
+  })
+})
