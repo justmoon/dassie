ae221dea2bb9fa2f77443c754fcaf1532479dbe2
Stefan Thomas <justmoon@members.fsf.org>
2024-04-02 11:49:58 -0400

    fix(dev): fix node links in node logs

--- packages/app-dev/src/frontend/components/shared/node-link/node-link.tsx
+++ packages/app-dev/src/frontend/components/shared/node-link/node-link.tsx
@@ -4,7 +4,7 @@ import { COLORS } from "@dassie/app-node/src/frontend/constants/palette"
 import { selectBySeed } from "@dassie/lib-logger"
 
 const NodeLink = ({ id }: { id: string }) => (
-  <Link style={{ color: selectBySeed(COLORS, id) }} href={`/nodes/${id}`}>
+  <Link style={{ color: selectBySeed(COLORS, id) }} href={`~/nodes/${id}`}>
     {id}
   </Link>
 )
