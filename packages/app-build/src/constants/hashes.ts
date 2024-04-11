bc0fa71ebb4ca1fd497ae74fafa7fb6cbfa56a92
Stefan Thomas <justmoon@members.fsf.org>
2024-04-11 10:55:57 -0400

    feat(build): load binary hashes from same file as docker

--- packages/app-build/src/constants/hashes.ts
+++ packages/app-build/src/constants/hashes.ts
@@ -1,38 +1,15 @@
-export const VERIFIED_HASHES: Record<string, string> = {
-  "https://nodejs.org/dist/v20.2.0/node-v20.2.0-linux-arm64.tar.xz":
-    "4ff85df72645e17b3d6253fd8eb45d9cdbed7c68b416ac770adb2fd85ac67e3d",
-  "https://nodejs.org/dist/v20.2.0/node-v20.2.0-linux-armv7l.tar.xz":
-    "1af63104e276269c1c312aeb8ba894716536ae323fb612d8177bdd1c6d713e9b",
-  "https://nodejs.org/dist/v20.2.0/node-v20.2.0-linux-x64.tar.xz":
-    "9257b5d5cc807d372da0d3574ce0e2d9b14c9510d7099081d30a464e5d4c0b2b",
-  "https://nodejs.org/dist/v20.3.1/node-v20.3.1-linux-arm64.tar.xz":
-    "75f820e7e0c460d902eb2c35716d158c06a4692e69f9a6cf2be30a721d7e0b42",
-  "https://nodejs.org/dist/v20.3.1/node-v20.3.1-linux-armv7l.tar.xz":
-    "55d405b0ce92fe85a2604c56e92757ba255fff698f7e7d1bc5c9a3f5efcc966c",
-  "https://nodejs.org/dist/v20.3.1/node-v20.3.1-linux-x64.tar.xz":
-    "a9f94435763f9c0128a8b6282ccbeefd0413a96e78e4427cfb7831d150c50334",
-  "https://nodejs.org/dist/v20.8.1/node-v20.8.1-linux-arm64.tar.xz":
-    "fec6edefa7ff959b29c7887735582ff2a2211b36a65a539da0f37db6797b7cff",
-  "https://nodejs.org/dist/v20.8.1/node-v20.8.1-linux-armv7l.tar.xz":
-    "f8370aaecd2cc2f26f8571aed7ffcf8efb6dc884a9a5e8e7a5e225e5ccfe6b74",
-  "https://nodejs.org/dist/v20.8.1/node-v20.8.1-linux-x64.tar.xz":
-    "44096f6276cf735f3b25f47ffaaa1629b0abad4d9932c3a77d9dcdc743a3ff92",
-  "https://github.com/WiseLibs/better-sqlite3/releases/download/v8.4.0/better-sqlite3-v8.4.0-node-v115-linux-arm64.tar.gz":
-    "944d537e729e690daf97c929e3da544e771e39e7d1fd16478a08c880a9eaa431",
-  "https://github.com/WiseLibs/better-sqlite3/releases/download/v8.4.0/better-sqlite3-v8.4.0-node-v115-linux-arm.tar.gz":
-    "1e6eaa0572ffadf022f69a3c3ca23e9e22c440e9a51dff844568928c92b9b716",
-  "https://github.com/WiseLibs/better-sqlite3/releases/download/v8.4.0/better-sqlite3-v8.4.0-node-v115-linux-x64.tar.gz":
-    "52c4862faab95b4c70bb1ad41a1e70a0d422447d8effd3a4e6547f1233307606",
-  "https://github.com/WiseLibs/better-sqlite3/releases/download/v9.0.0/better-sqlite3-v9.0.0-node-v115-linux-arm64.tar.gz":
-    "5cbcc578b6930ee40ce59285d8892fa62d527bed41d4849f7e4bc8248d78fa19",
-  "https://github.com/WiseLibs/better-sqlite3/releases/download/v9.0.0/better-sqlite3-v9.0.0-node-v115-linux-arm.tar.gz":
-    "d23d57d340b537f8351d47a66fc892d0d3300a65a3321e52b81b80b630f8b703",
-  "https://github.com/WiseLibs/better-sqlite3/releases/download/v9.0.0/better-sqlite3-v9.0.0-node-v115-linux-x64.tar.gz":
-    "e8d56a5064e839bc9b7ab84575eaaf16056aebcd0f08697cbdb1845c618a6e6d",
-  "https://github.com/WiseLibs/better-sqlite3/releases/download/v9.2.0/better-sqlite3-v9.2.0-node-v115-linux-arm64.tar.gz":
-    "d7f1a22c817b8eb53914a8d88dd7272ccd023c618f3f4faa159a3d0f9d96479f",
-  "https://github.com/WiseLibs/better-sqlite3/releases/download/v9.2.0/better-sqlite3-v9.2.0-node-v115-linux-arm.tar.gz":
-    "d5f9c5726b3311fcf2ec50af0f3f7bb6e85596e5c5fca0a4f737f098e1de5d1c",
-  "https://github.com/WiseLibs/better-sqlite3/releases/download/v9.2.0/better-sqlite3-v9.2.0-node-v115-linux-x64.tar.gz":
-    "7b6a881b5544d983da785df6fdfe1ece7a773cfe3af684ee2c27ebe4cedd085b",
-}
+import { readFileSync } from "node:fs"
+
+const binaryDependenciesText = readFileSync(
+  new URL("../../resources/binaries/binary-dependencies.txt", import.meta.url),
+  "utf8",
+)
+
+const binaryDependencies = binaryDependenciesText
+  .split("\n")
+  .map((line) => line.split(" "))
+  .filter((line) => line[0]!.startsWith("https://"))
+
+export const VERIFIED_HASHES: Record<string, string> = Object.fromEntries(
+  binaryDependencies.map(([url, hash]) => [url!, hash!]),
+)
