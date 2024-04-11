23fb379f04d91b1ba57c578be0003782af32e8ea
Stefan Thomas <justmoon@members.fsf.org>
2024-04-10 20:47:56 -0400

    fix(node): check if node identity is available before using it

--- packages/app-node/src/backend/logger/capture-logs.ts
+++ packages/app-node/src/backend/logger/capture-logs.ts
@@ -2,13 +2,15 @@ import { context as loggingContext } from "@dassie/lib-logger"
 import { type Reactor, createActor } from "@dassie/lib-reactive"
 
 import { LogsStore } from "../../common/stores/logs"
+import { HasNodeIdentitySignal } from "../config/computed/has-node-identity"
 import { NodeIdSignal } from "../ilp-connector/computed/node-id"
 
 export const CaptureLogsActor = (reactor: Reactor) => {
   const logsStore = reactor.use(LogsStore)
 
   return createActor((sig) => {
-    const nodeId = sig.readAndTrack(NodeIdSignal)
+    const hasNodeIdentity = sig.readAndTrack(HasNodeIdentitySignal)
+    const nodeId = hasNodeIdentity ? sig.readAndTrack(NodeIdSignal) : "none"
 
     loggingContext.output = (logEvent) => {
       if (logEvent.type === "clear") {
