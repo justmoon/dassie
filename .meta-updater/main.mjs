a38cb959581cdb2ec2ecb52c353279c9d91ac01f
Stefan Thomas <justmoon@members.fsf.org>
2024-04-22 04:36:20 -0400

    chore(meta): automatically set internal ts project references

--- .meta-updater/main.mjs
+++ .meta-updater/main.mjs
@@ -1,7 +1,17 @@
-import { existsSync, readdirSync } from "node:fs"
-import { join } from "node:path"
+import { readWantedLockfile } from "@pnpm/lockfile-file"
+import { createUpdateOptions } from "@pnpm/meta-updater"
 
-export default (workspaceDir) => {
+import { existsSync, readdirSync } from "node:fs"
+import { join, relative, resolve } from "node:path"
+
+export default createUpdateOptions(async (workspaceDir) => {
+  const lockfile = await readWantedLockfile(workspaceDir, {
+    ignoreIncompatible: false,
+  })
+  if (lockfile == null) {
+    throw new Error("no lockfile found")
+  }
+
   return {
     "package.json": (manifest, dir) => {
       return {
@@ -33,8 +43,29 @@ export default (workspaceDir) => {
         }
       }
 
+      const relativePath = relative(workspaceDir, dir)
+
+      const importer = lockfile.importers[relativePath]
+      if (!importer) return tsConfig
+      const deps = {
+        ...importer.dependencies,
+        ...importer.devDependencies,
+      }
+
+      const references = []
+
+      for (const [depName, spec] of Object.entries(deps)) {
+        if (!spec.startsWith("link:") || spec.length === 5) continue
+        const relativePath = spec.slice(5)
+        const linkedPkgDir = join(dir, relativePath)
+        if (!existsSync(join(linkedPkgDir, "tsconfig.json"))) continue
+        if (!resolve(linkedPkgDir).startsWith(resolve(workspaceDir))) continue
+        references.push({ path: relativePath })
+      }
+
       return {
         ...tsConfig,
+        references: references.sort((r1, r2) => r1.path.localeCompare(r2.path)),
         compilerOptions: {
           ...tsConfig.compilerOptions,
           outDir: "dist",
@@ -42,4 +73,4 @@ export default (workspaceDir) => {
       }
     },
   }
-}
+})
