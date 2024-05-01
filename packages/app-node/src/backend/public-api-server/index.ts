0444edc03ce673dbdb3ea6b008ff71354ba96b0d
Stefan Thomas <justmoon@members.fsf.org>
2024-05-01 10:36:18 -0400

    feat(node): add http endpoint for querying node list hash

--- packages/app-node/src/backend/public-api-server/index.ts
+++ packages/app-node/src/backend/public-api-server/index.ts
@@ -1,8 +1,10 @@
 import { createActor } from "@dassie/lib-reactive"
 
+import { RegisterNodeListMetadataHttpHandlerActor } from "./register-node-list-metadata-http-handler"
 import { RegisterStatisticsHttpHandlerActor } from "./register-statistics-http-handler"
 
 export const PublicApiServerActor = () =>
   createActor((sig) => {
     sig.run(RegisterStatisticsHttpHandlerActor)
+    sig.run(RegisterNodeListMetadataHttpHandlerActor)
   })
---
+++ packages/app-node/src/backend/public-api-server/register-node-list-metadata-http-handler.ts
@@ -0,0 +1,21 @@
+import { createJsonResponse } from "@dassie/lib-http-server"
+import { Reactor, createActor } from "@dassie/lib-reactive"
+
+import { HttpsRouter } from "../http-server/values/https-router"
+import { NodeListHashSignal } from "../peer-protocol/computed/node-list-hash"
+
+export const RegisterNodeListMetadataHttpHandlerActor = (reactor: Reactor) => {
+  const http = reactor.use(HttpsRouter)
+  const nodeListHashSignal = reactor.use(NodeListHashSignal)
+
+  return createActor((sig) => {
+    http
+      .get()
+      .path("/nodelist/meta")
+      .handler(sig, () => {
+        return createJsonResponse({
+          hash: nodeListHashSignal.read().toString("base64url"),
+        })
+      })
+  })
+}
