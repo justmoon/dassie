eaca37b48c123c72c195843f01c95592e6b7c3c1
Stefan Thomas <justmoon@members.fsf.org>
2024-05-17 14:01:02 -0400

    fix(node): duplicate logs

--- packages/app-node/src/command-line/commands/daemon/nodejs/log-to-console.ts
+++ packages/app-node/src/command-line/commands/daemon/nodejs/log-to-console.ts
@@ -1,22 +1,11 @@
-import { createCliFormatter } from "@dassie/lib-logger"
 import { type Reactor, createActor } from "@dassie/lib-reactive"
 
 import { AttachLogger } from "../../../../backend"
-import { LogsStore } from "../../../../common/stores/logs"
 
 export const LogToConsoleActor = (reactor: Reactor) => {
-  const logsStore = reactor.use(LogsStore)
   const attachLogger = reactor.use(AttachLogger)
 
-  const format = createCliFormatter()
-
-  return createActor((sig) => {
+  return createActor(() => {
     attachLogger()
-
-    logsStore.changes.on(sig, ([action, parameters]) => {
-      if (action === "addLogLine") {
-        process.stdout.write(format(parameters[0]) + "\n")
-      }
-    })
   })
 }
