3e3cb2fd2f0ab759f72f5f6791888cad93bdc4dc
Stefan Thomas <justmoon@members.fsf.org>
2024-05-18 19:29:12 -0400

    fix(node): serve all vite assets correctly

--- packages/app-node/src/backend/http-server/serve-frontend.ts
+++ packages/app-node/src/backend/http-server/serve-frontend.ts
@@ -1,19 +1,25 @@
 import { readFileSync } from "node:fs"
 import path from "node:path"
 
-import { type HttpResponse, createPlainResponse } from "@dassie/lib-http-server"
+import {
+  type HttpResponse,
+  createBinaryResponse,
+  createPlainResponse,
+} from "@dassie/lib-http-server"
 import { createActor } from "@dassie/lib-reactive"
 
 import type { DassieReactor } from "../base/types/dassie-base"
 import { EnvironmentConfig } from "../config/environment-config"
 import { HttpsRouter } from "./values/https-router"
 
-const ASSETS_REGEX = /^\/assets\/([\da-z]+-[\w-]{8}\.([a-z]{2,3}))$/
+const ASSETS_REGEX = /^\/((?:assets\/)?[\w-]+\.([a-z]{2,}))$/
 
 const MIME_TYPES_BY_EXTENSION: Record<string, string> = {
   ico: "image/x-icon",
+  png: "image/png",
   css: "text/css",
   js: "text/javascript",
+  webmanifest: "application/manifest+json",
 }
 
 export const ServeFrontendActor = (reactor: DassieReactor) => {
@@ -31,10 +37,10 @@ export const ServeFrontendActor = (reactor: DassieReactor) => {
       const contentType = MIME_TYPES_BY_EXTENSION[extension!]
       if (!contentType) return undefined
 
-      const assetPath = path.resolve(frontendPath, "assets", filename!)
+      const assetPath = path.resolve(frontendPath, filename!)
 
       try {
-        return createPlainResponse(readFileSync(assetPath, "utf8"), {
+        return createBinaryResponse(readFileSync(assetPath), {
           contentType,
         })
       } catch {
