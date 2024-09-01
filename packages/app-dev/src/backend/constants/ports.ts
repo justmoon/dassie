a7a054f5afcbe56f1c01f40cf8d05ec126234be3
Stefan Thomas <justmoon@members.fsf.org>
2024-09-01 15:13:13 -0400

    chore(dev): change dev server ports
    This change is to avoid conflicts with Lightning Polar
    (lightningpolar.com).

--- packages/app-dev/src/backend/constants/ports.ts
+++ packages/app-dev/src/backend/constants/ports.ts
@@ -1,8 +1,8 @@
 export const SNI_PROXY_PORT = 443
 
-export const DEBUG_UI_PORT = 10_000
-export const DEBUG_UI_RPC_PORT = 10_001
-export const DEBUG_RUNNER_RPC_PORT = 10_002
+export const DEBUG_UI_PORT = 10_100
+export const DEBUG_UI_RPC_PORT = 10_101
+export const DEBUG_RUNNER_RPC_PORT = 10_102
 
 export const NODES_START_PORT = 5001
 export const NODES_DEBUG_START_PORT = 6001
